c67477e3194b661ae442295b6409aedf46986984
yinyue200 <yinyue200@outlook.com>
2017-07-28 14:54:46 +0800

    add AuthingEventArgs

---
+++ src/OneDrive.Sdk.Authentication.XamarinForms/Ui/AuthingEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.OneDrive.Sdk.Authentication
+{
+    public class AuthingEventArgs:EventArgs
+    {
+        public Uri CallbackUri { get; set; }
+        public Uri RequestUri { get; set; }
+    }
+}
--- src/OneDrive.Sdk.Authentication.XamarinForms/Web/FormsWebAuthenticationUi.cs
+++ src/OneDrive.Sdk.Authentication.XamarinForms/Web/FormsWebAuthenticationUi.cs
@@ -7,8 +7,18 @@ namespace Microsoft.OneDrive.Sdk.Authentication
 {
     public class FormsWebAuthenticationUi : IWebAuthenticationUi
     {
+        public FormsWebAuthenticationUi()
+        {
+
+        }
+        public Xamarin.Forms.INavigation Navigation { get;private set; }
+        public FormsWebAuthenticationUi(Xamarin.Forms.INavigation navigation)
+        {
+            Navigation = navigation;
+        }
         public event EventHandler<AuthCompletedEventArgs> Completed;
         public event EventHandler<AuthFailedEventArgs> Failed;
+        public event EventHandler<AuthingEventArgs> Authing;
         public Task<IDictionary<string, string>> AuthenticateAsync(Uri requestUri, Uri callbackUri)
         {
             TaskCompletionSource<IDictionary<string, string>> tcs = new TaskCompletionSource<IDictionary<string, string>>();
@@ -23,9 +33,14 @@ namespace Microsoft.OneDrive.Sdk.Authentication
                 tcs.SetException(e.Error);
             };
 
+            Navigation?.PushAsync(new Ui.FormsWebAuthenticationPage(this, requestUri, callbackUri));
+            Authing?.Invoke(this, new AuthingEventArgs() { RequestUri = requestUri, CallbackUri = callbackUri });
+
 
             return tcs.Task;
         }
+
+
 
         internal void OnCompleted(AuthCompletedEventArgs e)
         {
